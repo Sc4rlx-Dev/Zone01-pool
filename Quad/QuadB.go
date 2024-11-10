05bb167dbfe8ace634be2b6ebdcf8faca977dfa1
oerraoui <itsscarl2022@gmail.com>
2024-11-10 18:01:58 +0100

    QuadB

---
+++ Quad/QuadB.go
@@ -0,0 +1,31 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadB(x, y int) {
+    if x <= 0 || y <= 0 {
+        return
+    }
+
+    xMax := x
+    yMax := y
+
+    for i := 1; i <= yMax; i++ {
+        for j := 1; j <= xMax; j++ {
+			if (i == 1 && j == 1) || (i == yMax && j == xMax){
+				z01.PrintRune('/')
+			}else if i == 1 && j == xMax{
+                z01.PrintRune('\\')
+            }else if i == yMax && j == 1{
+                z01.PrintRune('\\')
+            }else if i == 1 || i == yMax{
+                z01.PrintRune('*')
+            }else if j == 1 || j == xMax{
+                z01.PrintRune('*')
+            }else{
+                z01.PrintRune(' ')
+            }
+        }
+        z01.PrintRune('\n')
+    }
+}
--- Quad/main/main.go
+++ Quad/main/main.go
@@ -3,5 +3,5 @@ package main
 import "piscine"
 
 func main() {
-	piscine.QuadA(6,7)
+	piscine.QuadB(5,5)
 }
